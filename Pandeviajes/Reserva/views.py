e647955f620a51c5ddc38022f70bfae89abf9402
NicolasCoro <nico.coronado@alumnos.duoc.cl>
2020-10-20 20:08:18 -0300

    Proyecto con viws listas, htmls creados

--- Pandeviajes/Reserva/views.py
+++ Pandeviajes/Reserva/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from . models import Contacto, Reserva
+from django.views import generic
 
 # Create your views here.
+def index(request):
+    num_reserva= Reserva.objects.all().count()
+    num_contacto= Contacto.objects.all().count()
+
+    num_reserva_available=Reserva.objects.filter(destinos__exact='hawai').count()
+
+    return render(
+        request,
+        'index.html',
+        context={'Numero de reservas': num_reserva,'Numero de personas contacto:':num_contacto,
+                'Numero de reservas con destino=Hawai':num_reserva_available},
+    )
+
+
