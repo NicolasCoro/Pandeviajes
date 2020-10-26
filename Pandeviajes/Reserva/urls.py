ca94ae8cb95278045b73060ae42c6707c7465f8c
NicolasCoro <nico.coronado@alumnos.duoc.cl>
2020-10-26 01:30:24 -0300

    Formularios creados(falta html)

--- Pandeviajes/Reserva/urls.py
+++ Pandeviajes/Reserva/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('reserva/<int:pk', views.ReservaDetailView.as_view(), name='reserva_detail'),
+]
+urlpatterns+=[
+    path('reserva/create/', views.ReservaCreate.as_view(), name= 'reserva_create'),
+    path('reserva/<int:pk>/update/' ,views.ReservaUpdate.as_view(), name='reserva_update'),
+    path('reserva/<int:pk>/delete/',views.ReservaDelete.as_view(), name='reserva_delete'),
+
 ]
--- Pandeviajes/Reserva/views.py
+++ Pandeviajes/Reserva/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from . models import Contacto, Reserva
 from django.views import generic
 
+
 # Create your views here.
 def index(request):
     num_reserva= Reserva.objects.all().count()
@@ -15,5 +16,23 @@ def index(request):
         context={'Numero de reservas': num_reserva,'Numero de personas contacto:':num_contacto,
                 'Numero de reservas con destino=Hawai':num_reserva_available},
     )
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+class ReservaCreate(CreateView):
+    model= Reserva
+    fields= all
+    initial={'destinos': 'NA'}
+class ReservaUpdate(UpdateView):
+    model= Reserva
+    fields= all
+class ReservaDelete(DeleteView):
+    model = Reserva
+    success_url= reverse_lazy(Reserva)   
+class ReservaDetailView(generic.DetailView):
+    model: Reserva 
+
+
 
 
