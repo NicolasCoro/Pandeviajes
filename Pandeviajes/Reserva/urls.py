88e87a7be2ce026920734dd26f9de4017011daba
NicolasCoro <nico.coronado@alumnos.duoc.cl>
2020-11-01 07:57:11 -0300

    problema con reserva_list, reserva_detail sin esos no tenemos el delete

--- Pandeviajes/Reserva/urls.py
+++ Pandeviajes/Reserva/urls.py
@@ -3,13 +3,17 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
-    path('reserva/<int:pk>', views.ReservaDetailView.as_view(), name='reserva_detail'),
+    path('Destinos/', views.Destinos, name='Destinos'),
+    path('Pasajes/', views.Pasajes, name='Pasajes'),
+    
+    
+    path('Rese/<int:pk>', views.ReservaDetailView.as_view(), name='reserva_detail'),
 
-    path('Reserva/', views.ReservaListView.as_view(), name='Reservas')
+    path('Rese/', views.ReservaListView.as_view(), name='Reserva_list')
 ]
 urlpatterns+=[
-    path('reserva/create/', views.ReservaCreate.as_view(), name= 'reserva_create'),
-    path('reserva/<int:pk>/update/' ,views.ReservaUpdate.as_view(), name='reserva_update'),
-    path('reserva/<int:pk>/delete/',views.ReservaDelete.as_view(), name='reserva_delete'),
+    path('Rese/create/', views.ReservaCreate.as_view(), name= 'reserva_create'),
+    path('Rese/<int:pk>/update/' ,views.ReservaUpdate.as_view(), name='reserva_update'),
+    path('Rese/<int:pk>/delete/',views.ReservaDelete.as_view(), name='reserva_delete'),
 
 ]
--- Pandeviajes/Reserva/views.py
+++ Pandeviajes/Reserva/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from . models import Contacto, Reserva
+from . models import Reserva, Contacto
 from django.views import generic
 
 
-# Create your views here.
+# Create your views here. 
 def index(request):
     num_reserva= Reserva.objects.all().count()
     num_contacto= Contacto.objects.all().count()
@@ -13,25 +13,40 @@ def index(request):
     return render(
         request,
         'index.html',
-        context={'Numero de reservas': num_reserva,'Numero de personas contacto:':num_contacto,
-                'Numero de reservas con destino=Hawai':num_reserva_available},
+        context={'num_reserva': num_reserva,'num_contacto:':num_contacto,
+                'num_reserva_available':num_reserva_available},
     )
+def Destinos(request):
+
+    return render(
+        request,
+        'Destinos.html',
+    )
+def Pasajes(request):
+
+    return render(
+        request,
+        'Pasajes.html',
+    )
+
+
+    
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 class ReservaCreate(CreateView):
     model= Reserva
-    fields= all
+    fields= '__all__'
     initial={'destinos': 'NA'}
 class ReservaUpdate(UpdateView):
     model= Reserva
-    fields= all
+    fields= '__all__'
 class ReservaDelete(DeleteView):
     model = Reserva
     success_url= reverse_lazy(Reserva)   
 
-class ReservaListView(generic.ReservaListView):
+class ReservaListView(generic.ListView):
     model= Reserva
     paginate_by=10
 
